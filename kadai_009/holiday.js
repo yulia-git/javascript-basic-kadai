9daf1ca0d8ce64ed3f2b2ddf96155c8f25f2b401
yulia-git <157003332+yulia-git@users.noreply.github.com>
2024-02-12 10:14:08 +0900

    upload folder

---
+++ kadai_009/holiday.js
@@ -0,0 +1,10 @@
+holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+
+const n=holidays.length;
+for (let i=0;i<=n-1;i+=1){
+console.log(holidays[i]);}
+let i=0;
+while(i<n){
+console.log(holidays[i]);
+i+=1;
+}
